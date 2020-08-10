cf02b2f1abf36ba0f9562bb035f20bcb2b69cf68
benriis <benjaymin.p@gmail.com>
2020-08-10 10:32:39 +0200

    Started on navbar dev

---
+++ components/Navbar/Hamburger.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Hamburger.module.scss';
+
+const Hamburger = () => {
+  return (
+    <button className={styles.button}>
+      <div />
+      <div />
+      <div />
+    </button>
+  )
+}
+
+export default Hamburger;
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './Navbar.module.scss'
 import Link from 'next/link'
 import { isLoggedIn, getUser, logout } from '../../helpers/auth'
+import Hamburger from './Hamburger'
 
 const Navbar = () => {
 
@@ -14,7 +15,7 @@ const Navbar = () => {
   if (isLoggedIn()) {
     leftSideNav = (
     <>
-      <Link href="/page/new"><a className="btn-link">Write page</a></Link>
+      <Link href="/page/new"><a className="btn-link">+ Write page</a></Link>
       <a href="#" onClick={handleLogout} className="btn-link">logout</a>
       <p className={styles.greeter}>Hey {getUser()}</p>
     </>
@@ -33,7 +34,12 @@ const Navbar = () => {
       <div className={styles.header}>
         <Link href="/"><h1 className={styles.logo}>ACORN</h1></Link>
       </div>
-      {leftSideNav}
+      <div className={styles.leftSideWeb}>
+        {leftSideNav}
+      </div>
+      <div className={styles.leftSideMobile}>
+        <Hamburger />
+      </div>
     </nav>
   )
 }
